dcba165a193e35aa66a32eea23d4b1c7ebb77511
Rus Hughes <russell.hughes@gmail.com>
2018-05-19 23:12:21 +0700

    section 7 lecture 162

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Button } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
+import Layout from '../components/Layout';
 
 class CampaignIndex extends Component {
 
@@ -26,10 +27,27 @@ class CampaignIndex extends Component {
 
   render() {
 
-    return <div>
-      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
-      { this.renderCampaigns() }
+    return (
+      <Layout>
+      <div>
+        <link
+          rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
+        />
+        <h3>Open Campaigns</h3>
+
+        <Button
+          floated="right"
+          content="Create Campaign"
+          icon="add circle"
+          primary
+        />
+
+        { this.renderCampaigns() }
+
     </div>
+    </Layout>
+  )
   }
 }
 
